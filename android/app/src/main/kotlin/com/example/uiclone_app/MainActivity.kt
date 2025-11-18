4c246bb9e57f6f694eb7c1127b40d739bf3eb218
sova <mahatoshobha143@gmail.com>
2025-11-18 21:29:09 +0545

    initial commit

---
+++ android/app/src/main/kotlin/com/example/uiclone_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uiclone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:uiclone_app/views/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}

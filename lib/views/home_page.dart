abf462e1ac6d8610b678b5d9341f3ec5f5131e0f
sova <mahatoshobha143@gmail.com>
2025-11-18 21:36:23 +0545

    initial commit

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,7 +7,23 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(child: Text('Welcome to the Home Page!')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 200,
+            color: Colors.blue,
+            margin: EdgeInsets.all(16),
+            child: Center(
+              child: Text(
+                'This is a blue container',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ),
+          Center(child: Text('Welcome to the Home Page!')),
+        ],
+      ),
     );
   }
 }
